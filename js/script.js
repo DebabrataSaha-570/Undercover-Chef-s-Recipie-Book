aaee3657d7989064f861bc3f795db46a0e522a36
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-15 00:20:23 +0600

    modal added and background color added

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ const displayMeals = (meals) => {
         div.classList.add('cards')
 
         div.innerHTML = `
-            <div onClick="singleMealDetail('${meal.idMeal}')"  class="card h-100 ">
+            <div onClick="singleMealDetail('${meal.idMeal}')"  class="card h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title text-center">${meal.strMeal}</h5>
@@ -62,17 +62,82 @@ const singleMealDetail = (id) => {
 
 const displayMealDetail = (meal) => {
     console.log(meal)
-    const mealDetail = document.getElementById('meal-detail-container')
-    mealDetail.innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${meal.strMeal}</h5>
-      <p class="card-text">Meal Instruction : ${meal.strInstructions}</p>
-      <a href="${meal.strYoutube}" class="btn btn-primary">Youtube </a>
-    </div>
-  </div>
+    const mealDetails = document.getElementById('meal-details')
+    console.log(mealDetails)
+    mealDetails.innerHTML = `
+    <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center"  id="exampleModalLabel">${meal.strMeal}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <p> <span class="text-success">Recipe :</span> ${meal.strInstructions} </p>
+                    
+                    <table class="table table-bordered border-primary">
+                    <thead>
+                        <tr>
+        
+                            <th scope="col">Ingredients</th>
+        
+                            <th scope="col">Measurement</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+        
+                            <td>${meal.strIngredient1}</td>
+        
+                            <td>${meal.strMeasure1}</td>
+                        </tr>
+                        <tr>
+        
+                            <td>${meal.strIngredient2}</td>
+        
+                            <td>${meal.strMeasure2}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient3}</td>
+        
+                            <td>${meal.strMeasure3}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient4}</td>
+        
+                            <td>${meal.strMeasure4}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient5}</td>
+        
+                            <td>${meal.strMeasure5}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient6}</td>
+        
+                            <td>${meal.strMeasure6}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient7}</td>
+        
+                            <td>${meal.strMeasure7}</td>
+                        </tr>
+                        <tr>
+                            <td>${meal.strIngredient8}</td>
+        
+                            <td>${meal.strMeasure8}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <a target="_blank" href=" ${meal.strYoutube}"> Video Tutorial </a> 
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
     `
+
 }
+
 
 
